e7b8896e7dccf7173c3cca20ead2ebe20db2464b
Vilde Gjærum <vildegjaerum@gmail.com>
2020-02-21 08:46:25 +0100

    Update testing_trained_agents.py

--- testing_trained_agents.py
+++ testing_trained_agents.py
@@ -4,14 +4,9 @@ import Agent
 import matplotlib.pyplot as plt
 import random
 
-PLAY_GAME = 0
+PLAY_GAME = 1
 PLAY_ALL_GAMES = 0
 PLOT_TRAINING = 0
-
-boardShapes = ["Diamond", "Triangle"]
-boardSizes = [3,4]
-
-NUM_STEPS = 25
 
 BOARD_SHAPE = "Triangle" # or "Triangle"
 BOARD_SIZE = 5 #any number
@@ -44,7 +39,7 @@ def random_init(environment):
     placementHoles = random.sample(pop, np.random.randint(1,int(environment.get_obs_space()/1.5)))
     return placementHoles
 
-def show_policy(shape, size, placement_holes, agent):
+def show_policy(shape, size, placement_holes, agent,type):
     numHoles = len(placement_holes)
 
     t_states = []
@@ -62,141 +57,49 @@ def show_policy(shape, size, placement_holes, agent):
             break
     print(len(t_states))
     vis = Environment.VisualizePegSolitaire(env.get_org_boardState(),shape)
-    vis.show_played_game(t_states,t_actions,DELAY_BETWEEN_MOVE)
+    vis.show_played_game(t_states,t_actions,DELAY_BETWEEN_MOVE,shape,size,type)
 
 if PLAY_GAME:
-    shape = "Diamond"
-    size = 4
-    name = "D_4_tab.npy"
+    shape = BOARD_SHAPE
+    size = BOARD_SIZE
+    placementHoles = PLACEMENT_HOLES
+
+
+    name = str(shape+"_"+str(size)+"_"+CRITIC_TYPE+"_hole_place_"+str(placementHoles[0])+".npy")
+
     policy = np.load(name)
-    placementHoles = [9]
     numHoles = len(placementHoles)
-    PegSolitaire = Environment.PegSolitaire(shape,size,numHoles,placementHoles)
-    obs_space = PegSolitaire.get_obs_space()
-    random_inits = list(range(0,obs_space))
-    #print(random_inits)
-
-    agent = Agent.Agent("Tabular",[1,1,1], obs_space = obs_space, action_space = 6, gamma = 0.9, alpha = 0.01, lamda = 0.85)
+    env = Environment.PegSolitaire(shape,size,numHoles,placementHoles)
+    obs_space = env.get_obs_space()
+    agent = Agent.Agent(critic_type =CRITIC_TYPE, NN_structure=NN_STRUCTURE, obs_space= obs_space, action_space = 6 , critic_lerning_rate= CRITIC_LEARNING_RATE, actor_learning_rate = ACTOR_LEARNING_RATE, critic_e_decay_rate = CRITIC_E_DECAY_RATE, actor_e_decay_rate=ACTOR_E_DECAY_RATE, gamma=0.85)
+    #agent = Agent.Agent(critic_type[c],[25,25,25], obs_space = obs_space, action_space = 6, gamma = 0.9, lamda = 0.85)
     agent.set_policy(policy)
-    vis = Environment.VisualizePegSolitaire(PegSolitaire.get_org_boardState(),shape)
 
-    idx = np.random.randint(0,len(random_inits))
-    placementHoles=[random_inits[idx]]
+    show_policy(shape,size,placementHoles,agent,CRITIC_TYPE)
 
-    placementHoles = [0]
-    numHoles = len(placementHoles)
-    state =PegSolitaire.reset(numHoles=len(placementHoles), placementHoles = placementHoles)
-    vis.drawBoard(state,[None,None])
 
-    state= PegSolitaire.get_boardState()
 
-    for s in range(0,NUM_STEPS):
-        action =agent.get_action(state,0.0)
-        move_done, reward, state_next, ended = PegSolitaire.move_peg(action[0],action[1])
-        if not move_done:
-            print("ILLEGAL ACTION :( ", state, action)
-            break
-        else:
-            print("OK ACTION :)", state,action)
 
-            vis.drawBoard(PegSolitaire.get_org_boardState(), PegSolitaire.get_lastAction())
-            state = state_next
-            lastAction = action
 if PLAY_ALL_GAMES:
-    for shape in boardShapes:
-        for size in boardSizes:
-            print(shape," with size ",size)
-            name = shape+"_"+str(size)+"_tab_v2.npy"
-            policy = np.load(name)
-
-            placementHoles = [0]
-            numHoles = len(placementHoles)
-            PegSolitaire = Environment.PegSolitaire(shape,size,numHoles,placementHoles)
-            obs_space = PegSolitaire.get_obs_space()
-            #print(random_inits)
-
-            agent = Agent.Agent("Tabular",[1,1,1], obs_space = obs_space, action_space = 6, gamma = 0.9, alpha = 0.01, lamda = 0.85)
-            agent.set_policy(policy)
-
-
-            placementHoles = random_init(PegSolitaire)
-            numHoles = len(placementHoles)
-            state =PegSolitaire.reset(numHoles=len(placementHoles), placementHoles = placementHoles)
-            #vis.drawBoard(state,[None,None])
-
-            state= PegSolitaire.get_boardState()
-            states = []
-            states.append(PegSolitaire.get_org_boardState())
-            actions =[]
-            actions.append([None,None])
-            for s in range(0,NUM_STEPS):
-                action =agent.get_action(state,0.0)
-                move_done, reward, state_next, ended = PegSolitaire.move_peg(action[0],action[1])
-                if not move_done or ended:
-                    #print("ILLEGAL ACTION :( ", state, action)
-                    print(s-1," actions was taken")
-                    break
-                else:
-                    #print("OK ACTION :)", PegSolitaire.get_org_boardState(),action)
-                    states.append(PegSolitaire.get_org_boardState())
-                    #print("after appendend",states)
-                    actions.append(PegSolitaire.get_lastAction())
-                    #vis.drawBoard(PegSolitaire.get_org_boardState(), PegSolitaire.get_lastAction())
-                    state = state_next
-            print(states)
-            vis = Environment.VisualizePegSolitaire(PegSolitaire.get_org_boardState(),shape)
-            #vis.show_played_game(states,actions)
-
-
-
-
-if PLOT_TRAINING:
-
-        for shape in boardShapes:
-            for size in boardSizes:
-                name = shape+"_"+str(size)+"_tab_0.npy"
-                num_steps = np.load("episode_num_steps_"+name)
-                returns = np.load("episode_return_"+name)
-                n=100
-                num_steps_avg = [sum(num_steps[i:i+n])//n for i in range(0,len(num_steps),n)]
-                returns_avg = [sum(returns[i:i+n])//n for i in range(0,len(returns),n)]
-                plt.figure(figsize=(60,60))#figsize=(124, 248)
-
-                plt.subplot(131)
-                plt.plot(num_steps_avg)
-                plt.ylabel("Num steps avg every "+str(n)+" episode.")
-
-                plt.subplot(132)
-                plt.plot(returns_avg)
-                plt.ylabel("Return avg every "+str(n)+" episode.")
-                plt.suptitle(shape+" "+ str(size))
-                plt.show()
-
-if True:
-    game_type=[["Diamond",4,9],["Diamond",4,6],["Triangle",5,4],["Triangle",5,7],["Triangle",5,8]]
+    game_type=[["Diamond",4,10],["Diamond",4,5],["Triangle",5,4],["Triangle",5,7],["Triangle",5,8]]
     critic_type=["NN","Tabular"]
+
     for g in range(0,len(game_type)):
         for c in range(0,len(critic_type)):
-            name2=str(game_type[g][0]+"_"+str(game_type[g][2])+"__board_size"+str(game_type[g][1])+"_"+critic_type[c]+".npy")
+            shape = game_type[g][0]
+            size = game_type[g][1]
+            placementHoles = [game_type[g][2]]
+            type = critic_type[c]
+            name2 = str(shape+"_"+str(size)+"_"+type+"_hole_place_"+str(placementHoles[0])+".npy")
+            #name2=str(game_type[g][0]+"_"+str(game_type[g][2])+"__board_size"+str(game_type[g][1])+"_"+critic_type[c]+".npy")
             print(name2)
             policy = np.load(name2)
+            if True:
+                numHoles = len(placementHoles)
+                env = Environment.PegSolitaire(shape,size,numHoles,placementHoles)
+                obs_space = env.get_obs_space()
+                agent = Agent.Agent(critic_type =type, NN_structure=NN_STRUCTURE, obs_space= obs_space, action_space = 6 , critic_lerning_rate= CRITIC_LEARNING_RATE, actor_learning_rate = ACTOR_LEARNING_RATE, critic_e_decay_rate = CRITIC_E_DECAY_RATE, actor_e_decay_rate=ACTOR_E_DECAY_RATE, gamma=0.85)
+                #agent = Agent.Agent(critic_type[c],[25,25,25], obs_space = obs_space, action_space = 6, gamma = 0.9, lamda = 0.85)
+                agent.set_policy(policy)
 
-            placementHoles = [game_type[g][2]]
-            numHoles = len(placementHoles)
-            env = Environment.PegSolitaire(game_type[g][0],game_type[g][1],numHoles,placementHoles)
-            obs_space = env.get_obs_space()
-            agent = Agent.Agent(critic_type =critic_type[c], NN_structure=NN_STRUCTURE, obs_space= obs_space, action_space = 6 , critic_lerning_rate= CRITIC_LEARNING_RATE, actor_learning_rate = ACTOR_LEARNING_RATE, critic_e_decay_rate = CRITIC_E_DECAY_RATE, actor_e_decay_rate=ACTOR_E_DECAY_RATE, gamma=0.85)
-            #agent = Agent.Agent(critic_type[c],[25,25,25], obs_space = obs_space, action_space = 6, gamma = 0.9, lamda = 0.85)
-            agent.set_policy(policy)
-
-            show_policy(game_type[g][0],game_type[g][1],placementHoles,agent)
-    game_type=[["Diamond",4,9],["Diamond",4,6],["Triangle",5,4],["Triangle",5,7],["Triangle",5,8]]
-    critic_type=["NN","Tabular"]
-    for g in range(0,len(game_type)):
-        for c in range(0,len(critic_type)):
-            name = game_type[g][0]+"_boardsize_"+ str(game_type[g][1])+"_"+ str(game_type[g][2])+"_"+ critic_type[c]
-            learning_plot =np.load(str(name + "_learning_plot.npy"))
-            #agent.save_policy(str(game_type[g][0]+"_"+ str(game_type[g][2])),str("_board_size")+str(game_type[g][1]), critic_type[c])
-            plt.plot(learning_plot)
-            plt.show()
-            plt.close()
+                show_policy(shape,size,placementHoles,agent,type)
